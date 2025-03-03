eef041f962557738a9fbf9d06e95f9fdd01dcace
LMS-Ou-Academy <lmsouacademy@gmail.com>
2025-03-03 20:48:17 +0500

    first commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-import {clerkwebhooks} from './controllers/webhooks.js'
+import clerkwebhooks from './controllers/webhooks.js'
 
 // Initialize Express
 const app = express()
