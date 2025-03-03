521e70b62467408391cb4fdef93e786d479dbb7b
LMS-Ou-Academy <lmsouacademy@gmail.com>
2025-03-03 21:47:44 +0500

    first commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-import clerkwebhooks from './controllers/webhooks.js'
+import clerkWebhooks from './controllers/webhooks.js'
 
 // Initialize Express
 const app = express()
@@ -15,7 +15,7 @@ app.use(cors())
 
 // Routes
 app.get('/', (req,res)=>res.send("API Working"))
-app.post('/clerk', express.json(), clerkwebhooks)
+app.post('/clerk', express.json(), clerkWebhooks)
 
 // PORT
 const PORT = process.env.PORT || 5000
